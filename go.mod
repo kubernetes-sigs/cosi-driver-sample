97aeacc1b0cc82748ad7d413cc1c9a2d16c2d063
Nicolas Trangez <ikke@nicolast.be>
2021-05-05 14:47:42 +0000

    pkg: implement `identityserver` package

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/scality/cosi-provisioner-sample
+
+go 1.15
+
+require (
+	github.com/onsi/ginkgo v1.16.2
+	github.com/onsi/gomega v1.11.0
+	sigs.k8s.io/container-object-storage-interface-spec v0.0.0-20210330184956-b0de747ccee4
+)
---
+++ pkg/identityserver/identityserver_test.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Kubernetes Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identityserver_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	spec "sigs.k8s.io/container-object-storage-interface-spec"
+
+	identityserver "github.com/scality/cosi-provisioner-sample/pkg/identityserver"
+)
+
+var _ = Describe("IdentityServer", func() {
+	const (
+		provisionerName = "cosi-provisioner-sample-test"
+	)
+
+	var (
+		ctx            context.Context
+		identityServer spec.IdentityServer
+	)
+
+	BeforeEach(func() {
+		ctx = context.TODO()
+		identityServer = identityserver.NewIdentityServer(provisionerName)
+	})
+
+	It("Correctly implements ProvisionerGetInfo", func() {
+		resp, err := identityServer.ProvisionerGetInfo(ctx, &spec.ProvisionerGetInfoRequest{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(resp.Name).To(Equal(provisionerName))
+	})
+})
